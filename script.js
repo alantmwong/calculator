6bc772dbec8b0f237556503963addf0eca2d2f0f
alantmwong <95393427+alantmwong@users.noreply.github.com>
2023-01-29 15:59:20 +1300

    added the 4 basic operations as functions

---
+++ script.js
@@ -0,0 +1,15 @@
+add = (firstNumber, secondNumber) => {
+    return firstNumber + secondNumber;
+};
+
+subtract = (firstNumber, secondNumber) => {
+    return firstNumber - secondNumber;
+};
+
+multiply = (firstNumber, secondNumber) => {
+    return firstNumber * secondNumber;
+};
+
+divide = (firstNumber, secondNumber) => {
+    return firstNumber / secondNumber;
+}
