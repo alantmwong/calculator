feeb3169ec2b995969b3e99bc578b4eca9fdf922
alantmwong <95393427+alantmwong@users.noreply.github.com>
2023-01-29 16:17:02 +1300

    linked css file and added operate function

--- script.js
+++ script.js
@@ -12,4 +12,17 @@ multiply = (firstNumber, secondNumber) => {
 
 divide = (firstNumber, secondNumber) => {
     return firstNumber / secondNumber;
-}
+};
+
+operate = (operator, firstNumber, secondNumber) => {
+    if (operator == "+") {
+        return add(firstNumber, secondNumber);
+    } else if (operator == "-") {
+        return subtract(firstNumber, secondNumber);
+    } else if (operator == "*") {
+        return multiply(firstNumber, secondNumber);
+    } else if (operator == "/") {
+        return divide(firstNumber, secondNumber);
+    }
+};
+
